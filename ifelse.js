52ba8bf75c3b3225729ef386f0c662bf80cf4451
Alex95221 <romulopereiraalmeida@gmail.com>
2025-07-28 23:08:12 -0300

    if e else

---
+++ ifelse.js
@@ -0,0 +1,7 @@
+const senhaDoUsuario = 12365
+const senhaDigitada = 12365
+if (senhaDoUsuario == senhaDigitada) {
+    console.log("Seja bem-vindo, você está Logado(a)")
+} else  {
+    console.log("Sua Senha está incorreta, tente novamente!")
+}
